870cc0dbe8bca64610b0820b35143a1a44e779a3
mettashalini89@gmail.com <CatMatDog@123>
2023-03-28 15:00:17 -0600

    environment with db and apps

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+
+  parameters = [var.component]
+}
--- vars.tf
+++ vars.tf
@@ -6,3 +6,6 @@ variable "allow_subnets" {}
 variable "vpc_id" {}
 variable "dns_domain" {}
 variable "bastion_cidr" {}
+variable "component" {
+  default = "rabbitmq"
+}
