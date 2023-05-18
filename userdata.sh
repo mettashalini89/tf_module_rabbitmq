6dc0abefe17eeb634ab11819a6b8a8c10755da31
mettashalini89@gmail.com <CatMatDog@123>
2023-05-17 23:33:31 -0600

    kubernets practice

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+labauto ansible
 ansible-pull -i localhost, -U https://github.com/mettashalini89/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
